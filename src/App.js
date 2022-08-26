e8e31dd0fb3916f150ed0fd3e376827ac50ec8a3
cyc4528 <cyc4528@gmail.com>
2022-08-26 15:22:42 +0900

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg'
-import './App.css'
+import "./styles.css";
+import React from "react";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Switch, FormControl, FormLabel } from "@chakra-ui/react";
 
-const App = () => {
+import { CircularProgress, CircularProgressLabel } from "@chakra-ui/react";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton
+} from "@chakra-ui/react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon
+} from "@chakra-ui/react";
+import {
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup
+} from "@chakra-ui/react";
+
+export default function App() {
+  const [data, setData] = React.useState(null);
+  React.useEffect(() => {
+    const asdf = async () => {
+      const resp = await fetch("https://api.github.com/users/cyc4528");
+      const json = await resp.json();
+      setData(json);
+    };
+    asdf();
+  }, []);
+
+  if (data === null) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
-}
+    <>
+      <Button colorScheme="green">asasdf</Button>
+      <FormControl display="flex" alignItems="center">
+        <FormLabel htmlFor="banana" mb="0">
+          혜민이를 싫어하십니까?
+        </FormLabel>
+        <Switch id="banana" />
+      </FormControl>
+      <Stat>
+        <StatLabel>Collected Fees</StatLabel>
+        <StatNumber>£0.00</StatNumber>
+        <StatHelpText>Feb 12 - Feb 28</StatHelpText>
+      </Stat>
+      <StatGroup>
+        <Stat>
+          <StatLabel>Sent</StatLabel>
+          <StatNumber>345,670</StatNumber>
+          <StatHelpText>
+            <StatArrow type="increase" />
+            23.36%
+          </StatHelpText>
+        </Stat>
 
-export default App
+        <Stat>
+          <StatLabel>Clicked</StatLabel>
+          <StatNumber>45</StatNumber>
+          <StatHelpText>
+            <StatArrow type="decrease" />
+            9.05%
+          </StatHelpText>
+        </Stat>
+      </StatGroup>
+      <Tabs>
+        <TabList>
+          <Tab>One</Tab>
+          <Tab>Two</Tab>
+          <Tab>Three</Tab>
+          <Tab>Four</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <p>하나</p>
+          </TabPanel>
+          <TabPanel>
+            <p>둘</p>
+          </TabPanel>
+          <TabPanel>
+            <p>셋</p>
+          </TabPanel>
+          <TabPanel>
+            <p>넷</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+      <CircularProgress value={30} size="150px" />
+      <CircularProgress value={40} color="green.400">
+        <CircularProgressLabel>90%</CircularProgressLabel>
+      </CircularProgress>
+      <CircularProgress isIndeterminate color="green.300" />
+    </>
+  );
+}
